bc3648ed1d69751dc711e187aa5cc133c3ff5cd8
Dusan Juranovic <juranovicd@gmail.com>
2017-12-07 23:15:45 +0000

    Initial Commit

---
+++ TeamworkTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamworkTestApp
+//
+//  Created by Dusan Juranovic on 12/7/17.
+//  Copyright © 2017 Dusan Juranovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
